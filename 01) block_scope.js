c5d4d7244f8aa5f7ed7fb99ec1679d0fa6b610e1
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-10-06 17:50:56 +0200

    Update 01) block_scope.js

--- 01) block_scope.js
+++ 01) block_scope.js
@@ -1,11 +1,20 @@
-// Block Scope
-function test(x){
+// Einsatz von var
   var i;
-  console.log(i); 
+  console.log(i);  // undefined
   if(x){
     var i = 5;
   }
-  console.log(i);
+  console.log(i); // 5
 }
 test(5);
 
+// Einsatz von let
+function test(x){
+  var i;
+  console.log(i); // undefined
+  if(x){
+    let i = 5;
+  }
+  console.log(i); // undefined
+}
+test(5);
