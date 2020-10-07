945f85d12e458a019ee4838bd7e62d22c5932bbb
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-10-07 17:17:24 +0200

    Update 11) template_strings.js

--- 11) template_strings.js
+++ 11) template_strings.js
@@ -4,3 +4,9 @@ const vorName = 'Peter';
 console.log('Vorname: ' + vorName + ' Nachname: ' + name); // Vorname: Max Nachname: Mustermann
 // Neue Schreibweise
 console.log(`Vorname: ${vorName} Nachname: ${name}`); // Vorname: Max Nachname: Mustermann
+
+// Mehrzeilige Zeichenkette
+const text = `Mehrzeilige Nachricht
+ohne definierten
+Zeilenumbrung`
+ console.log(text);
