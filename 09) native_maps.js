f42913006832fbb5bee848a04347323661c690ca
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-10-07 15:49:53 +0200

    Create 09) native_maps.js

---
+++ 09) native_maps.js
@@ -0,0 +1,8 @@
+// Native Map-Funktion ES2015
+// clear, delete, get, has, set, size, entries, keys, values
+const filme = new Map();
+filme.set('ES', 2004);
+filme.set('Pocahontas', 2001);
+filme.set('MIB', 2008);
+
+console.log(filme.get('MIB')); // 2008
