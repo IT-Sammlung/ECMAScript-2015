1d757122aa06d73c5920f4c4d26d3d2c757e3014
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-10-07 17:12:43 +0200

    Create 11) template_strings.js

---
+++ 11) template_strings.js
@@ -0,0 +1,6 @@
+// Alte Schreibweise
+const name = 'Mustermann';
+const vorName = 'Peter';
+console.log('Vorname: ' + vorName + ' Nachname: ' + name); // Vorname: Max Nachname: Mustermann
+// Neue Schreibweise
+console.log(`Vorname: ${vorName} Nachname: ${name}`); // Vorname: Max Nachname: Mustermann
